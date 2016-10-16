9eff977366ba9d59a52d3f1ca59e25ea0a1d7d82
Funcke <funckejonas@gmail.com>
2016-10-16 12:17:08 +0200

    Update directory.c

--- directory.c
+++ directory.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <dir.h>
 #include <errno.h>
-#include <sys\stat.h>
-#include <windows.h>
-#include "killnewline.h"
+//include file for errorhandling
+#ifdef __unix__
+//includes for Unix systems
+	#include <sys/stat.h>
+#else
+//includes for windows systems
+	#include <sys\stat.h>
+#endif
+
 int main(void){
-	char dirname[10000];
+	char dirpath[10000];
+        //string wich holds the directory
 	int errorhandl;
-	fgets(dirname, 10000, stdin);
-	if(dirname!=NULL){
-		killnewline(dirname);
-		errorhandl=mkdir(dirname);
+        //integer for catching the reutrn of mkdir
+        
+	fgets(dirpath, 10000, stdin);
+        
+	if(dirpath!=NULL){
+                //testing if fgets was successful
+		dirpath[strlen-2]='\0';
+               //deleting the newline
+                
+		errorhandl=mkdir(dirpath);
+                //creating the directory
+                
 		if(errorhandl==0){
+                        //testing errno for an error
 			printf("Sucessful");
 		}else{
 			printf("%s", strerror(errno));
 		}
+                
 	}else{
 		printf("Fehler bei Stringeingabe");
 	}
+        
 	getchar();
+        //giving the user time to read the output
+	
 	return 0;
 }
